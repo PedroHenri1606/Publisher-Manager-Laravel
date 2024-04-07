e31c74fcba0833d9ca75e51b93502eed292a87e5
Pedro Henrique <pedrohenri1606@gmail.com>
2024-04-07 19:34:45 -0300

    Adding the Chart .js Library to the Project

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -4,13 +4,42 @@
 
 @section('content')
 
+
 <div class= "container tabela border-light shadow">
     <div class="container">
         <div class="opcoes text-center">
             Reports
         </div>
-        <p>Produzir relatorios do tipo pizza e graficos metricos aqui</p>
+         <div>
+          <canvas id="myChart"></canvas>
+        </div>
     </div>
 </div>
 
 @endsection
+
+@section('script')
+  <script>
+    const ctx = document.getElementById('myChart');
+
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  </script>
+@endsection
+

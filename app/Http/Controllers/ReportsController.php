b654fbc0ccb36ff3f4cbc7bd43274ba74f5214a6
Pedro Henrique <pedrohenri1606@gmail.com>
2024-04-10 11:50:57 -0300

    Added business rules to report screens

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Domain;
+use App\Models\Publisher;
 use App\Models\RevenueDomain;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ReportsController extends Controller
 {
@@ -14,10 +16,30 @@ class ReportsController extends Controller
     }
 
     public function historic(Domain $domain){
+
+        $user = Auth::user();
+
+        //se o usuario autenticado for publisher
+        if(Publisher::where('email', $user->email)->exists()){
+            $publisher = Publisher::where('email', $user->email)->first();
+
+            //se o publisher_id do domain informado for diferente do id do publisher, encerra a função e envia novamente para a reports.index
+            if($domain->publisher_id != $publisher->id){
+                return redirect()->route('reports.index');
+            
+            } else {
+                $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
         
-        $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
+                return view("reports.historic", ['revenuesDomain' => $revenuesDomain, 'domain' => $domain]);
+            }
         
-        return view("reports.historic", ['revenuesDomain' => $revenuesDomain, 'domain' => $domain]);
+        //se o usuario autenticado for um admin
+        } else {
+        
+            $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
+            
+            return view("reports.historic", ['revenuesDomain' => $revenuesDomain, 'domain' => $domain]);
+        }
     }
 
     public function create(Domain $domain){
@@ -54,7 +76,7 @@ class ReportsController extends Controller
     }
 
     public function show(Domain $domain){
-
+        
         $revenueDomain = new RevenueDomain;
 
         $cpmAverage = 0;
@@ -66,51 +88,122 @@ class ReportsController extends Controller
         $valoresCpmGrafico = '';  $indiceCpmGrafico = '';
         $valoresRevenueGrafico = '';  $indiceRevenueGrafico = '';
         
-        //Se o domain possuir um historic log, ira enviar revenue com os dados do domain
-        if(RevenueDomain::where('domain_id', $domain->id)->exists()){
+        $user = Auth::user();
 
-            //Valores presentes no grafico CPM
-            $valoresCpmGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('cpm')->toArray();
+        //se o usuario autenticado for publisher
+        if(Publisher::where('email', $user->email)->exists()){
+            $publisher = Publisher::where('email', $user->email)->first();
 
-                $indiceCpmGrafico = range(1, count($valoresCpmGrafico));
-                $indiceCpmGrafico = implode(',', $indiceCpmGrafico);
+            //se o publisher_id do domain informado for diferente do id do publisher, encerra a função e envia novamente para a reports.index
+            if($domain->publisher_id != $publisher->id){
+                return redirect()->route('reports.index');
+     
+            } else {    
 
-            $valoresCpmGrafico = implode(',', $valoresCpmGrafico);
+                //Se o domain possuir um historic log, ira enviar revenue com os dados do domain
+                if(RevenueDomain::where('domain_id', $domain->id)->exists()){
 
-            //Valores presentes no grafico Revenue
-            $valoresRevenueGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('revenue')->toArray();
+                    //Valores presentes no grafico CPM
+                    $valoresCpmGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('cpm')->toArray();
 
-                $indiceRevenueGrafico = range(1, count($valoresRevenueGrafico));
-                $indiceRevenueGrafico = implode(',', $indiceRevenueGrafico);
+                        $indiceCpmGrafico = range(1, count($valoresCpmGrafico));
+                        $indiceCpmGrafico = implode(',', $indiceCpmGrafico);
 
-            $valoresRevenueGrafico = implode(',', $valoresRevenueGrafico);
+                    $valoresCpmGrafico = implode(',', $valoresCpmGrafico);
 
-            //Valores presentes nos cards de informações do site
-            $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
+                    //Valores presentes no grafico Revenue
+                    $valoresRevenueGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('revenue')->toArray();
 
-            foreach($revenuesDomain as $indice => $valor){
-                $cpmAverage += $revenuesDomain[$indice]->cpm / $revenuesDomain->count();
-                    $cpmAverage = number_format($cpmAverage,2);
+                        $indiceRevenueGrafico = range(1, count($valoresRevenueGrafico));
+                        $indiceRevenueGrafico = implode(',', $indiceRevenueGrafico);
 
-                $rpmAverage += $revenuesDomain[$indice]->rpm / $revenuesDomain->count();
-                    $rpmAverage = number_format($rpmAverage,2);
+                    $valoresRevenueGrafico = implode(',', $valoresRevenueGrafico);
 
-                if($revenuesDomain[$indice]->revenue > $totalRevenue){
-                    $totalRevenue += $revenuesDomain[$indice]->revenue;
-                }
+                    //Valores presentes nos cards de informações do site
+                    $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
 
-                if($revenuesDomain[$indice]->impressions > $totalImpressions){
-                    $totalImpressions += $revenuesDomain[$indice]->impressions;
+                    foreach($revenuesDomain as $indice => $valor){
+                        $cpmAverage += $revenuesDomain[$indice]->cpm / $revenuesDomain->count();
+                            $cpmAverage = number_format($cpmAverage,2);
+
+                        $rpmAverage += $revenuesDomain[$indice]->rpm / $revenuesDomain->count();
+                            $rpmAverage = number_format($rpmAverage,2);
+
+                        if($revenuesDomain[$indice]->revenue > $totalRevenue){
+                            $totalRevenue += $revenuesDomain[$indice]->revenue;
+                        }
+
+                        if($revenuesDomain[$indice]->impressions > $totalImpressions){
+                            $totalImpressions += $revenuesDomain[$indice]->impressions;
+                            
+                        }
+                    }
+
+                    $totalImpressions = (number_format($totalImpressions,0,',','.'));
+                    $totalRevenue = (number_format($totalRevenue,2,',','.'));
+
+                    return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
+                
+                } else{
+
+                    //Se o domain não possui um historic log, ire enviar os dados zerados
+                    return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
                 }
             }
-
-            return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
         
-        } else{
+        //se o usuario autenticado for um admin
+        } else {
 
-            //Se o domain não possui um historic log, ire enviar os dados zerados
-            return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
-        }
+            //Se o domain possuir um historic log, ira enviar revenue com os dados do domain
+            if(RevenueDomain::where('domain_id', $domain->id)->exists()){
+
+                //Valores presentes no grafico CPM
+                $valoresCpmGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('cpm')->toArray();
+
+                    $indiceCpmGrafico = range(1, count($valoresCpmGrafico));
+                    $indiceCpmGrafico = implode(',', $indiceCpmGrafico);
+
+                $valoresCpmGrafico = implode(',', $valoresCpmGrafico);
+
+                //Valores presentes no grafico Revenue
+                $valoresRevenueGrafico = RevenueDomain::where('domain_id', $domain->id)->pluck('revenue')->toArray();
+
+                    $indiceRevenueGrafico = range(1, count($valoresRevenueGrafico));
+                    $indiceRevenueGrafico = implode(',', $indiceRevenueGrafico);
+
+                $valoresRevenueGrafico = implode(',', $valoresRevenueGrafico);
+
+                //Valores presentes nos cards de informações do site
+                $revenuesDomain = RevenueDomain::where('domain_id', $domain->id)->get();
+
+                foreach($revenuesDomain as $indice => $valor){
+                    $cpmAverage += $revenuesDomain[$indice]->cpm / $revenuesDomain->count();
+                        $cpmAverage = number_format($cpmAverage,2);
+
+                    $rpmAverage += $revenuesDomain[$indice]->rpm / $revenuesDomain->count();
+                        $rpmAverage = number_format($rpmAverage,2);
+
+                    if($revenuesDomain[$indice]->revenue > $totalRevenue){
+                        $totalRevenue += $revenuesDomain[$indice]->revenue;
+                    }
+
+                    if($revenuesDomain[$indice]->impressions > $totalImpressions){
+                        $totalImpressions += $revenuesDomain[$indice]->impressions;
+                        
+                    }
+                }
+
+                $totalImpressions = (number_format($totalImpressions,0,',','.'));
+                $totalRevenue = (number_format($totalRevenue,2,',','.'));
+
+                return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
+            
+            } else{
+
+                //Se o domain não possui um historic log, ire enviar os dados zerados
+                return view('reports.show', compact('domain','totalImpressions','totalRevenue','cpmAverage','rpmAverage','valoresCpmGrafico','indiceCpmGrafico','valoresRevenueGrafico','indiceRevenueGrafico'));
+            }
+        } 
     }
 
     public function deleteRevenue(RevenueDomain $revenueDomain){
